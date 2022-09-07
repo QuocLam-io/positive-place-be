202c9c291afe7b2ad01c23f3adf57fb84fe6a0ac
frostycakes <lamquoc.24@gmail.com>
2022-09-07 14:18:06 -0400

    menu close button css

--- src/Components/MobileMenu.jsx
+++ src/Components/MobileMenu.jsx
@@ -13,7 +13,9 @@ const MobileMenu = ({ menuCloseHandler, logOutHandler, isDarkMode }) => {
     >
       <div className="menu-close-btn">
         <p onClick={menuCloseHandler}>Close</p>
-        <p className="menu-close-btn" onClick={menuCloseHandler}>x</p>
+        <button 
+        className={`${isDarkMode && "menu-parent-dark"}`}
+        onClick={menuCloseHandler}>x</button>
       </div>
       <div className="menu-links">
         <div className="menu-link" onClick={menuCloseHandler}>
