467b46eb6596557062541979fdebcf53337d5c30
frostycakes <lamquoc.24@gmail.com>
2022-09-07 11:17:47 -0400

    menu light background

--- src/Components/MobileMenu.jsx
+++ src/Components/MobileMenu.jsx
@@ -2,15 +2,16 @@ import React from "react";
 import { motion } from "framer-motion";
 
 const MobileMenu = ({ menuCloseHandler }) => {
-  return(  <motion.div
-    className="menu-parent"
-    onClick={menuCloseHandler}
-    initial={{ opacity: 0 }}
-    animate={{ opacity: 1 }}
-    exit={{ opacity: 0 }}
-  >
-    hello
-  </motion.div>
+  return (
+    <motion.div
+      className="menu-parent"
+      onClick={menuCloseHandler}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+    >
+      hello
+    </motion.div>
   );
 };
 
